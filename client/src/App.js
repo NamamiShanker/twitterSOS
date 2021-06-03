2ab8e0e1cf4b8259c33cb12e527ceda85f4ac074
Arpit Maurya <arpitchatting@gmail.com>
2021-06-03 18:02:43 +0530

    fix codacy static analysis issues

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>Hello world</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
